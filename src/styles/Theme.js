bf3bb2b3e38e12d81c77b0b23c489eeb0e2f7033
gyuhyuk <bruno3@naver.com>
2023-07-01 12:15:02 +0900

    setting: GlobalStyle 설정

---
+++ src/styles/Theme.js
@@ -0,0 +1,12 @@
+export const Theme = {
+  colors: {
+    WHITE: "#FFFFFF",
+    GRAY: "#D9D9D9",
+    BLACK: "#000000",
+    PURPLE10: "#BF30E21A",
+    PURPLE30: "#BF30E24D",
+    PURPLE50: "#BF30E280",
+    PURPLE70: "#BF30E2B2",
+    PURPLE100: "#BF30E2",
+  },
+};
