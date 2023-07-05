3fd571af3d46ca6555409ab746d94ed1649bba87
kimdongyoung <wwweric12@naver.com>
2023-07-05 21:20:45 +0900

    feat: input-component 구현

---
+++ src/assets/component/Input.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export default function Input({ placeholder, type, register, inputId }) {
+  return (
+    <InputContainer>
+      <InputBox
+        placeholder={placeholder}
+        type={type}
+        // {...register(inputId)}
+        id={inputId}
+      />
+    </InputContainer>
+  );
+}
+
+const InputContainer = styled.div`
+  width: 100%;
+  height: 35px;
+  padding: 7px 15px;
+  border-radius: 10px;
+  border: 1px solid ${({ theme }) => theme.colors.PURPLE70};
+`;
+
+const InputBox = styled.input`
+  width: 100%;
+  border: none;
+  font-size: 16px;
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.GRAY};
+    font-weight: 600;
+  }
+`;
