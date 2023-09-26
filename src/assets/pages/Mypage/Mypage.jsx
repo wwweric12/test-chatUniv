8b0e32309d6a337aeab47b5ab2049a8a7ba3ec58
unknown <pih9134@naver.com>
2023-09-26 23:56:13 +0900

    fix/내정보수정Delete

--- src/assets/pages/Mypage/Mypage.jsx
+++ src/assets/pages/Mypage/Mypage.jsx
@@ -4,7 +4,6 @@ import styled from "styled-components";
 
 const Mypage = () => {
   const [myPageData, seMyPageData] = useState([
-    { path: "editprofile", title: "내정보 수정" },
     { path: "boardlist", title: "게시물내역 조회" },
     { path: "mychatlist", title: "채팅내역 조회" },
     { path: "comment", title: "댓글내역 조회" },
